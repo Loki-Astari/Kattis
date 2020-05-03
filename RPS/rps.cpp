e8ace82eb6fcb5fa345ddce7375bff01bd1a46dd
Loki Astari <Loki.Astari@gmail.com>
2020-05-03 14:15:14 -0700

    Version 4 of RPS

--- RPS/rps.cpp
+++ RPS/rps.cpp
@@ -13,6 +13,37 @@ static int const result[4][4] = {
 };
 
 
+static constexpr std::size_t bufferSize = 28;
+std::vector<char>   data(bufferSize + 1);
+char*               buffer  = &data[0];
+char*               theend  = buffer;
+
+template<typename...T>
+bool getData(char const* format, T... args)
+{
+    std::size_t size = 0;
+    //fprintf(stdout, "Left: %ld, Pre: --->%s<---\n", (theend - buffer), buffer);
+    if ((buffer == theend) || (sscanf(buffer, format, args..., &size) != sizeof...(T)) || (buffer + size >= theend))
+    {
+        //fprintf(stdout, "Size: %lu Before: --->%s<---\n", size, buffer);
+        size = theend - buffer;
+        std::copy(buffer, buffer + size, &data[0]);
+        std::size_t max = fread(&data[size], 1, bufferSize - size, stdin);
+        max += size;
+        buffer = &data[0];
+        buffer[max] = '\0';
+        //fprintf(stdout, "Had: %lu Got: %lu Now: --->%s<---\n", size, max, buffer);
+        theend = buffer + max;
+        if (sscanf(buffer, format, args..., &size) != sizeof...(T)) {
+            //fprintf(stdout, "After0: --->%s<---\n", buffer);
+            return false;
+        }
+        //fprintf(stdout, "After1: --->%s<---\n", buffer);
+    }
+    buffer += size;
+    return true;
+}
+
 int main()
 {
     //std::chrono::time_point<std::chrono::system_clock> start = std::chrono::system_clock::now();
@@ -23,47 +54,41 @@ int main()
         int wins = 0;
         int loss = 0;
     };
-    std::vector<char>   data(5'000);
-    std::size_t max = fread(&data[0], 1, 5'000, stdin);
+    //std::size_t max = fread(&data[0], 1, bufferSize, stdin);
     //fprintf(stdout, "Read: %lu: %c %c\n", max, data[0], data[2]);
 
-    char* buffer = &data[0];
-    buffer[max] = '\0';
-    char* theend = buffer + max;
-    int   size;
+    //buffer = &data[0];
+    //buffer[max] = '\0';
+    //theend = buffer + max;
 
     char lineBreak[] = "\0";
     while (true)
     {
         int n = 0;
-        sscanf(buffer, " %d%n", &n, &size);
-        buffer += size;
+        getData(" %d%n", &n);
 
-        if (n == 0 || buffer >= theend) {
+        if (n == 0) {
             break;
         }
 
         int k;
-        if (sscanf(buffer, " %d%n", &k, &size) == 1)
+        if (getData(" %d%n", &k))
         {
-            //fprintf(stdout, "Doing: %d => %d\n", n, k);
-            buffer += size;
             std::vector<T> games(n);
 
             fprintf(stdout, "%s", lineBreak);
             lineBreak[0] = '\n';
 
-            for (int gameCount = (k * n * (n -1))/2; gameCount; --gameCount) {
+            for (int gameCount = (k * n * (n -1))/2; gameCount; --gameCount)
+            {
                 //fprintf(stdout, "GC: %d\n", gameCount);
                 int         p1;
                 int         p2;
                 char        n1[20];
                 char        n2[20];
-                if (sscanf(buffer, " %d %s %d %s%n", &p1, n1, &p2, n2, &size) == 4)
+                //if (sscanf(buffer, " %d %s %d %s%n", &p1, n1, &p2, n2, &size) == 4)
+                if (getData(" %d %s %d %s%n", &p1, n1, &p2, n2))
                 {
-                    //fprintf(stdout, "%d %s V %d %s\n", p1, n1, p2, n2);
-                    buffer += size;
-
                     p1--;
                     p2--;
 
@@ -92,4 +117,5 @@ int main()
     //std::chrono::time_point<std::chrono::system_clock> end = std::chrono::system_clock::now();
     //std::cout << "Time: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "\n";
 }
+
 
