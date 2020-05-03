4036223d5b40549f14e6114121e23aee1f498d85
Loki Astari <Loki.Astari@gmail.com>
2020-05-03 14:14:48 -0700

    Version 2 of RPS

--- RPS/rps.cpp
+++ RPS/rps.cpp
@@ -5,7 +5,7 @@
 
 
 static int const result[4][4] = {
-//    p      r   s
+    //    p      r   s
     { 0, 0,  1, -1},     // paper
     { 0, 0,  0,  0},
     {-1, 0,  0,  1},     // rock
@@ -15,15 +15,17 @@ static int const result[4][4] = {
 
 int main()
 {
+     std::chrono::time_point<std::chrono::system_clock> start = std::chrono::system_clock::now();
     std::ios_base::sync_with_stdio(false);
+#ifndef NO_TIE
     std::cin.tie(nullptr);
+#endif
 
     struct T {
         int wins = 0;
         int loss = 0;
     };
-    std::vector<T> games;
-    games.reserve(100);
+
 
     char lineBreak[] = "\0";
     while (true)
@@ -43,51 +45,56 @@ int main()
 #ifdef USE_SCANF
         if (fscanf(stdin, " %d", &k) == 1)
 #else
-        if (std::cin >> k)
+            if (std::cin >> k)
 #endif
-        {
-            games.clear();
-            games.resize(n);
+            {
+                std::vector<T> games(n);
 
-            lineBreak[0] = '\n';
+                std::cout << lineBreak;
+                lineBreak[0] = '\n';
 
-            for (int gameCount = (k * n * (n -1))/2; gameCount; --gameCount) {
-                char        n1[20];
-                char        n2[20];
-                int         p1;
-                int         p2;
+                for (int gameCount = (k * n * (n -1))/2; gameCount; --gameCount) {
+                    int         p1;
+                    int         p2;
 #ifdef USE_SCANF
-                if (fscanf(stdin, " %d %s %d %s", &p1, n1, &p2, n2) == 4)
+                    char        n1[20];
+                    char        n2[20];
+                    if (fscanf(stdin, " %d %s %d %s", &p1, n1, &p2, n2) == 4)
 #else
-                if (std::cin >> p1 >> n1 >> p2 >> n2)
+                    std::string n1;
+                    std::string n2;
+
+                    if (std::cin >> p1 >> n1 >> p2 >> n2)
 #endif
-                {
-                    p1--;
-                    p2--;
-
-                    int v = result[n1[0] & 3][n2[0] & 3];
-                    //std::cout << p1 << " " << n1 << " V " << p2 << " " << n2 << " => " << v << "\n";
-
-                    if (v != 0)
                     {
-                        games[(v == 1) ? p1 : p2].wins++;
-                        games[(v == 1) ? p2 : p1].loss++;
+                        p1--;
+                        p2--;
+
+                        int v = result[n1[0] & 3][n2[0] & 3];
+                        //std::cout << p1 << " " << n1 << " V " << p2 << " " << n2 << " => " << v << "\n";
+
+                        if (v != 0)
+                        {
+                            games[(v == 1) ? p1 : p2].wins++;
+                            games[(v == 1) ? p2 : p1].loss++;
+                        }
+                    }
+                }
+
+                std::cout << std::fixed;
+                std::cout << std::setprecision(3);
+                for (auto const& game: games)
+                {
+                    int numOfWins  = game.wins;
+                    int numOfGames = game.wins + game.loss;
+                    if (numOfGames != 0) {
+                        std::cout << (1.0 * numOfWins / numOfGames) << "\n";
+                    } else {
+                        std::cout << "-\n";
                     }
                 }
             }
-
-            std::cout << std::fixed;
-            std::cout << std::setprecision(3);
-            for (auto const& game: games)
-            {
-                int numOfWins  = game.wins;
-                int numOfGames = game.wins + game.loss;
-                if (numOfGames != 0) {
-                    std::cout << (1.0 * numOfWins / numOfGames) << "\n";
-                } else {
-                    std::cout << "-\n";
-                }
-            }
-        }
     }
+    std::chrono::time_point<std::chrono::system_clock> end = std::chrono::system_clock::now();
+    std::cout << "Time: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "\n";
 }
