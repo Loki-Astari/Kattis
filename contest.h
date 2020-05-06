50680e514a56849f544ce23d91e4a7106d1ab9a8
Loki Astari <Loki.Astari@gmail.com>
2020-05-05 21:24:23 -0700

    Thread Pool
        A very simple thread pool and barrier.

--- contest.h
+++ contest.h
@@ -3,6 +3,11 @@
 
 #include <chrono>
 #include <iostream>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <vector>
+#include <deque>
 
 namespace ThorsAnvil::Contest
 {
@@ -35,6 +40,11 @@ class FastInt
         int c;
         while (std::isspace(c = buf->sbumpc()))
         {}
+
+        if (c == EOF) {
+            str.setstate(std::ios::eofbit);
+            return str;
+        }
 
         if (c == '-') {
             sign = -1;
@@ -74,6 +84,114 @@ class FastString
             //std::cerr << "Got: " << data.val << "\n";
 
             return str;
+        }
+};
+
+class Barrier
+{
+    std::condition_variable             variable;
+    int                                 count;
+    int                                 waiting;
+    public:
+        Barrier(int count)
+            : count(count)
+            , waiting(0)
+        {}
+
+        void waitFor(std::unique_lock<std::mutex>& lock)
+        {
+            waiting = 0;
+            variable.wait(lock);
+        }
+
+        void checkin(std::unique_lock<std::mutex>& lock)
+        {
+            waiting++;
+
+            if (waiting != count)
+            {
+                variable.wait(lock);
+            }
+            else
+            {
+                variable.notify_all();
+            }
+        }
+};
+
+class ThreadPool
+{
+    std::mutex                          mutex;
+    std::condition_variable             variable;
+    std::vector<std::thread>            threads;
+    std::deque<std::function<void()>>   jobs;
+    Barrier                             barrier;
+    bool                                finished;
+    public:
+        ThreadPool(int size = 4)
+            : barrier(size)
+            , finished(false)
+        {
+            std::unique_lock<std::mutex>   lock(mutex);
+
+            for(int loop = 0; loop < 4; ++loop) {
+                threads.emplace_back([&](){threadAction();});
+            }
+        }
+        ~ThreadPool()
+        {
+            {
+                std::unique_lock<std::mutex>   lock(mutex);
+                finished = true;
+            }
+            for(auto& thread: threads) {
+                addJob([](){});
+            }
+            for(auto& thread: threads) {
+                thread.join();
+            }
+        }
+        void addJob(std::function<void()>&& action)
+        {
+            {
+                std::unique_lock<std::mutex>   lock(mutex);
+                jobs.push_back(std::move(action));
+            }
+            variable.notify_one();
+        }
+        void waitForJobsToDrain()
+        {
+            std::unique_lock<std::mutex>   lock(mutex);
+            for(int loop = 0; loop < threads.size(); ++loop) {
+                jobs.emplace_back([&](){
+                    std::unique_lock<std::mutex>   lock(mutex);
+                    barrier.checkin(lock);
+                });
+            }
+            barrier.waitFor(lock);
+        }
+    private:
+        void threadAction()
+        {
+            while (!finished)
+            {
+                auto action = getAction();
+                action();
+            }
+        }
+        std::function<void()> getAction()
+        {
+            std::unique_lock<std::mutex>   lock(mutex);
+            variable.wait(lock, [&](){return !jobs.empty() || finished;});
+
+            if (finished) {
+                return [](){};
+            }
+
+            auto result = std::move(jobs.front());
+            jobs.pop_front();
+
+            return result;
         }
 };
 
